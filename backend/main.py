27f539904077103bfbc6f0bd73b7c05c260e7d0e
m <kanodecat@gmail.com>
2025-03-16 00:57:30 -0400

    removed more rust stuff and did things in backend

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from google import genai
 from dotenv import load_dotenv
 from flask_cors import CORS
 import os
+import moviepy.editor as mp
 
 load_dotenv()
 
@@ -17,44 +18,45 @@ app = Flask(__name__)
 
 CORS(app)
 
-@app.route("/results", methods=["POST"])
+@app.route("/results", methods=["GET"])
 def home():
-    with open('test.mp3', 'rb') as f:
-      image_bytes = f.read()
+    with open('test.mp3', 'rb') as f: # fix this I need to use webm
+        clip = mp.VideoFileClip(f)
+        image_bytes = clip.read()
 
     response = client.models.generate_content(
       model='gemini-2.0-flash',
       contents=[
-        f'''The audio file should be evaluated for public speaking based on the following criteria:
+        '''The audio file should be evaluated for public speaking based on the following criteria:
         Tone: Is the tone engaging and varied, or is it monotonous?
         Speaking Speed: Was the speaking speed appropriate or too fast/slow?
         Filler Words: How many filler words (like "um", "uh", "you know", etc.) are present?
         Volume: Was the speaker loud enough?
         The result should be provided in the following JSON format:
 
-        {{
+        {
           "score": 86,
-          "metrics": {{
-            "filler_words": {{
+          "metrics": {
+            "filler_words": {
               "count": 2,
               "status": "pass",
               "message": "You used less than 3 filler words"
-            }},
-            "tone": {{
+            },
+            "tone": {
               "status": "fail",
               "message": "Your tone was too monotone"
-            }},
-            "speed": {{
+            },
+            "speed": {
               "status": "fail",
               "message": "You spoke too fast",
               "words_per_minute": 180
-            }},
-            "volume": {{
+            },
+            "volume": {
               "status": "pass",
               "message": "You spoke loud enough"
-            }}
-          }}
-        }}
+            }
+          }
+        }
 
         Score: A rating out of 100 based on the overall evaluation.
         Metrics: A set of categories with:
@@ -96,7 +98,7 @@ def home():
 
     print(response)
 
-    return jsonify(sigma)
+    return jsonify(response)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000, debug=True)
