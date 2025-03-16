c993e7948f492d08d2584dc263221f0c89c9f2fb
jaedon farr <jaedonfarr@gmail.com>
2025-03-15 22:40:51 -0400

    small sigma

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import { Button } from "@radix-ui/themes";
-import VideoRecorder from "./components/input/main";
+import VideoRecorder from "../components/input/main";
 
 export default function Home() {
   return (
     <>
-  
-    <VideoRecorder/>
+      <VideoRecorder />
     </>
   );
 }
